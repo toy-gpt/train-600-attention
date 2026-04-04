283ace1e3991871963d7fc6886de0dd92688dd92
Denise Case <denisecase@gmail.com>
2026-04-03 19:52:23 -0500

    update

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,16 +2,16 @@
 
 import pytest
 
-from toy_gpt_train.c_model import EmbeddingNextTokenModel
+from toy_gpt_train.c_model import AttentionNextTokenModel
 
 
 def test_forward_output_sums_to_one():
-    model = EmbeddingNextTokenModel(vocab_size=10, embedding_dim=4, context_size=2)
+    model = AttentionNextTokenModel(vocab_size=10, embedding_dim=4, context_size=2)
     probs = model.forward([0, 1])
     assert abs(sum(probs) - 1.0) < 1e-6
 
 
 def test_forward_context_size_mismatch_raises():
-    model = EmbeddingNextTokenModel(vocab_size=10, embedding_dim=4, context_size=2)
+    model = AttentionNextTokenModel(vocab_size=10, embedding_dim=4, context_size=2)
     with pytest.raises(ValueError):
         model.forward([0])
